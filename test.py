9c1eda5dd26e51473669ed1f93bc73b376568950
niheeth03 <cs20b058@iittp.ac.in>
2023-03-13 01:27:22 +0530

    For loop with errors

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 i = 0
 n = int(input("No"))
-while i < n:
-    i *=23
-    i+=1
-    i*=3
-    j = 1
-    while j < n:
-        j+=1
-print("Largest number in the list:", largest_num)
+while j<n:
+    j+=1
+    while i < n:
+        i *=23
+        i+=1
+        i*=3
+
+
+        
